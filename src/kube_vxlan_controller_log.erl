ca542b4c026faf4f302f772ca418f91306b91348
Anton Alferov <anton.alferov@travelping.com>
2018-02-16 15:03:09 +0100

    Improve log module
    Ability to log using format and args

--- src/kube_vxlan_controller_log.erl
+++ src/kube_vxlan_controller_log.erl
@@ -1,22 +1,30 @@
 -module(kube_vxlan_controller_log).
+-compile({no_auto_import,[error/2]}).
 
 -export([
-    info/1,
-    warning/1,
-    error/1
+    info/1, info/2,
+    warning/1, warning/2,
+    error/1, error/2
 ]).
 
--define(LogFormat, "~s~4..0b-~2..0b-~2..0bT~2..0b:~2..0b:~2..0b.~3..0bZ ~p~n").
+-define(LogFormat, "~s~4..0b-~2..0b-~2..0bT~2..0b:~2..0b:~2..0b.~3..0bZ ~s~n").
+-define(TermFormat, "~p").
 
-info(Term) -> log(info, Term).
-warning(Term) -> log(warning, Term).
-error(Term) -> log(error, Term).
+info(Term) -> info(?TermFormat, [Term]).
+info(Format, Args) -> log(info, Format, Args).
 
-log(Type, Term) ->
+warning(Term) -> warning(?TermFormat, [Term]).
+warning(Format, Args) -> log(warning, Format, Args).
+
+error(Term) -> error(?TermFormat, [Term]).
+error(Format, Args) -> log(error, Format, Args).
+
+log(Type, Format, Args) ->
     Now = {_MegaSecs, _Secs, Mcs} = erlang:timestamp(),
     {{Y, Mo, D}, {H, Mi, S}} = calendar:now_to_universal_time(Now),
-    Args = [log_type(Type), Y, Mo, D, H, Mi, S, Mcs div 1000, Term],
-    io:format(?LogFormat, Args).
+    LogArgs = [log_type(Type), Y, Mo, D, H, Mi, S, Mcs div 1000,
+               lists:flatten(io_lib:format(Format, Args))],
+    io:format(?LogFormat, LogArgs).
 
 log_type(Type) -> case Type of
     info -> "I";
