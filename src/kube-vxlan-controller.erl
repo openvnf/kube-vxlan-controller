f1411f84192a26fef2b0b08e0513f4034a1e3002
Anton Alferov <anton.alferov@travelping.com>
2018-02-16 01:01:16 +0100

    Implementing event processing loop

--- src/kube-vxlan-controller.erl
+++ src/kube-vxlan-controller.erl
@@ -1,14 +1,17 @@
 -module('kube-vxlan-controller').
 
--export([main/1, run/0]).
+-export([main/1, run/1]).
 
--define(Host, "api.k8s.nce-01.fra-01.eu.cennso.net").
+-define(K8s, kube_vxlan_controller_k8s_client).
+-define(Log, kube_vxlan_controller_log).
+
+-define(Server, "https://api.k8s.nce-01.fra-01.eu.cennso.net").
+-define(CaCertFile, "pki/ca.pem").
+-define(TokenFile, "pki/token").
+
 -define(WatchApi, "/api/v1/watch/pods?labelSelector=vxlan%3Dtrue").
 
 %{ok, S} = rtmtb_api_usr_core:connect("https://api.k8s.nce-01.fra-01.eu.cennso.net/api/v1/namespaces/aalferov/pods/alpine1-594d4c669f-d27t4/exec?command=ls&command=-l&container=alpine1&container=alpine1&stderr=true&stdout=true", Headers), rtmtb_api_usr_core:recv(S).
-
--define(CaCertFile, "pki/ca.pem").
--define(TokenFile, "pki/token").
 
 -define(A8nVxlanNames, 'vxlan.travelping.com/names').
 -define(A8nVxlanNamesSep, ", \n").
@@ -26,84 +29,88 @@
   }
 }).
 
--define(LogFormat, "~s~4..0b-~2..0b-~2..0bT~2..0b:~2..0b:~2..0b.~3..0bZ ~p~n").
-
 %curl -X PATCH --cacert pki/ca.pem https://api.k8s.nce-01.fra-01.eu.cennso.net/apis/apps/v1beta2/namespaces/aalferov/deployments/alpine1 -H "Content-Type: application/strategic-merge-patch+json" --header "Authorization: Bearer $(cat pki/token)" -d '{"spec":{"template":{"spec":{"containers":[{"name":"vxlan-controller-agent","image":"alpine"}]}}}}'
 
 main(_) ->
+    Config = #{
+        server => ?Server,
+        token => binary_to_list(element(2, file:read_file(?TokenFile))),
+        ca_cert_file => ?CaCertFile
+    },
     application:ensure_all_started(?MODULE),
-    run().
+    run(Config).
 
-run() -> run(?Host, ?TokenFile, ?CaCertFile).
-
-run(Host, TokenFile, CaCertFile) ->
-    Url = "https://" ++ Host ++ ?WatchApi,
-    {ok, Token} = file:read_file(TokenFile),
-
-    Headers = [{"Authorization", "Bearer " ++ binary_to_list(Token)}],
-    Options = [
-        {ssl_options, [{cacertfile, CaCertFile}]},
-        {recv_timeout, infinity}
-    ],
-    
-    case hackney:request(get, Url, Headers, <<>>, Options) of
-        {ok, 200, _, Ref} -> run_loop(Ref, <<>>);
-        Other -> log(error, Other)
+run(Config) ->
+    case ?K8s:http_stream_request(
+        "/api/v1/watch/pods",
+        [{"labelSelector", "vxlan=true"}],
+        Config
+    ) of
+        {ok, Stream} -> run(Stream, Config);
+        {error, Reason} -> ?Log:error(Reason)
     end.
 
-run_loop(Ref, IncompleteData) ->
-    case hackney:stream_body(Ref) of
-        done -> ok;
-        {ok, NewData} ->
-            Data = <<IncompleteData/binary, NewData/binary>>,
-            run_loop(Ref, process_stream(binary:split(Data, <<"\n">>)));
-        {error, Reason} ->
-            log(error, Reason)
+run(Stream, Config) ->
+    case ?K8s:http_stream_read(Stream) of
+        {ok, done} -> run(Config);
+        {ok, Events} ->
+            process_events(Events, Config),
+            run(Stream, Config);
+        {error, Reason} -> ?Log:error(Reason)
     end.
 
-process_stream([Item|T]) when T /= [] ->
-    process_item(Item),
-    process_stream(T);
+process_events(Events, Config) ->
+    lists:foreach(process_event_fun(Config), Events).
 
-process_stream([<<>>]) -> <<>>;
-process_stream([Item]) ->
-    case binary:last(Item) == $\n of
-        true -> process_item(Item), <<>>;
-        false -> Item
-    end.
-
-process_item(Item) ->
-    Json = <<"[", Item/binary, "]">>,
-    Event = hd(jsx:decode(Json, [return_maps, {labels, atom}])),
-    log(info, event_read(Event)).
-
-event_read(Event = #{
+process_event_fun(Config) -> fun(Event = #{
     type := Type,
     object := #{
         metadata := #{
+            namespace := Namespace,
             uid := Uid,
+            name := Name,
             annotations := Annotations
         },
         status := Status
     }
 }) ->
     VxlanNames = btl(maps:get(?A8nVxlanNames, Annotations, <<>>)),
-    Event.
-%   {list_to_atom(string:lowercase(btl(Type))),
-%    btl(Uid),
-%    btl(maps:get(podIP, Status, <<>>)),
-%    string:lexemes(VxlanNames, ?A8nVxlanNamesSep)}.
-
-log(Type, Term) ->
-    Now = {_MegaSecs, _Secs, Mcs} = erlang:timestamp(),
-    {{Y, Mo, D}, {H, Mi, S}} = calendar:now_to_universal_time(Now),
-    Args = [log_type(Type), Y, Mo, D, H, Mi, S, Mcs div 1000, Term],
-    io:format(?LogFormat, Args).
-
-log_type(Type) -> case Type of
-    info -> "I";
-    warning -> "W";
-    error -> "E"
+    %?Log:info(Event)
+    process_event(#{
+        event => list_to_atom(string:lowercase(btl(Type))),
+        namespace => btl(Namespace),
+        uid => btl(Uid),
+        name => btl(Name),
+        pod_ip => btl(maps:get(podIP, Status, <<>>)),
+        vxlan_names => string:lexemes(VxlanNames, ?A8nVxlanNamesSep),
+    }, Config)
 end.
+
+process_event(Ev = #{namespace := Namespace, name := Name}, Config) ->
+    ?Log:info(patch_pod(Namespace, Name, Config)).
+
+patch_pod(Namespace, PodName, Config) ->
+    ResourcePod = "/api/v1/namespaces/" ++ Namespace ++
+                  "/pods/" ++ PodName,
+    {ok, [#{
+      metadata := #{
+        ownerReferences := [#{
+          kind := <<"ReplicaSet">>,
+          name := ReplicaSetName
+        }|_]
+      }
+    }]} = ?K8s:http_request(ResourcePod, [], Config),
+
+    ResourceReplicaSet = "/apis/extensions/v1/namespaces/" ++ Namespace ++
+                         "/replicasets/" ++ btl(ReplicaSetName),
+    {ok, [#{
+      metadata := #{
+        ownerReferences := [#{
+          kind := <<"Deployment">>,
+          name := DeploymentName
+        }|_]
+      }
+    }]} = ?K8s:http_request(ResourceReplicaSet, [], Config),
+    ?Log:info(DeploymentName).
 
 btl(B) -> binary_to_list(B).
