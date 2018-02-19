e4dc564f1912006e7762389aff1acba8792e400a
Anton Alferov <anton.alferov@travelping.com>
2018-02-19 18:00:24 +0100

    Watch event timeout correct processing and refactoring

--- src/kube-vxlan-controller.erl
+++ src/kube-vxlan-controller.erl
@@ -1,133 +1,105 @@
 -module('kube-vxlan-controller').
 
--export([main/1, run/1]).
+-export([main/1, run/2, config/0]).
 
 -define(K8s, kube_vxlan_controller_k8s_client).
+-define(Pod, kube_vxlan_controller_pod).
 -define(Log, kube_vxlan_controller_log).
 
 -define(Server, "https://api.k8s.nce-01.fra-01.eu.cennso.net").
 -define(CaCertFile, "pki/ca.pem").
 -define(TokenFile, "pki/token").
 
--define(WatchApi, "/api/v1/watch/pods?labelSelector=vxlan%3Dtrue").
-
-%{ok, S} = rtmtb_api_usr_core:connect("https://api.k8s.nce-01.fra-01.eu.cennso.net/api/v1/namespaces/aalferov/pods/alpine1-594d4c669f-d27t4/exec?command=ls&command=-l&container=alpine1&container=alpine1&stderr=true&stdout=true", Headers), rtmtb_api_usr_core:recv(S).
-
--define(A8nVxlanNames, 'vxlan.travelping.com/names').
--define(A8nVxlanNamesSep, ", \n").
-
--define(AgentSpec, #{
-  spec => #{
-    template => #{
-      spec => #{
-        containers => [
-          #{name => "kube-vxlan-controller-agent",
-            image => "aialferov/kube-vxlan-controller-agent"}
-        ]
-      }
-    }
-  }
-}).
-
--define(AgentExecQuery, [
-    {"container", "alpine1"},
-    {"stdout", "true"},
-    {"stderr", "true"}
-]).
-
-%curl -X PATCH --cacert pki/ca.pem https://api.k8s.nce-01.fra-01.eu.cennso.net/apis/apps/v1beta2/namespaces/aalferov/deployments/alpine1 -H "Content-Type: application/strategic-merge-patch+json" --header "Authorization: Bearer $(cat pki/token)" -d '{"spec":{"template":{"spec":{"containers":[{"name":"vxlan-controller-agent","image":"alpine"}]}}}}'
+-define(AgentContainerName, "vxlan-controller-agent").
 
 main(_) ->
-    Config = #{
-        server => ?Server,
-        token => binary_to_list(element(2, file:read_file(?TokenFile))),
-        ca_cert_file => ?CaCertFile
-    },
     application:ensure_all_started(?MODULE),
-    run(Config).
+    run(config(), #{}).
 
-run(Config) ->
-    case ?K8s:http_stream_request(
-        "/api/v1/watch/pods",
-        [{"labelSelector", "vxlan=true"}],
-        Config
-    ) of
-        {ok, Stream} -> run(Stream, Config);
+config() -> #{
+    server => ?Server,
+    token => binary_to_list(element(2, file:read_file(?TokenFile))),
+    ca_cert_file => ?CaCertFile,
+    agent_container_name => ?AgentContainerName
+}.
+
+run(Config, State) ->
+    ?Log:info("Resource version: ~s", [resource_version(State)]),
+    Resource = "/api/v1/watch/pods",
+    Query = [
+        {"labelSelector", "vxlan=true"},
+        {"resourceVersion", resource_version(State)},
+        {"timeoutSeconds", "30"}
+    ],
+    case ?K8s:http_stream_request(Resource, Query, Config) of
+        {ok, Stream} -> run(Stream, Config, State);
         {error, Reason} -> ?Log:error(Reason)
     end.
 
-run(Stream, Config) ->
+run(Stream, Config, State) ->
     case ?K8s:http_stream_read(Stream) of
-        {ok, done} -> run(Config);
+        {ok, done} -> run(Config, State);
         {ok, Events} ->
-            process_events(Events, Config),
-            run(Stream, Config);
+            NewState = process_events(Events, Config, State),
+            run(Stream, Config, NewState);
+        {error, timeout} -> run(Config, State);
         {error, Reason} -> ?Log:error(Reason)
     end.
 
-process_events(Events, Config) ->
-    lists:foreach(process_event_fun(Config), Events).
+process_events(Events, Config, State) ->
+    lists:foldl(process_event_fun(Config), State, Events).
 
-process_event_fun(Config) -> fun(Event = #{
-    type := Type,
-    object := #{
-        metadata := #{
-            namespace := Namespace,
-            uid := Uid,
-            name := Name,
-            annotations := Annotations
-        },
-        status := Status
-    }
-}) ->
-    VxlanNames = btl(maps:get(?A8nVxlanNames, Annotations, <<>>)),
-    %?Log:info(Event)
-    process_event(#{
-        event => list_to_atom(string:lowercase(btl(Type))),
-        namespace => btl(Namespace),
-        uid => btl(Uid),
-        name => btl(Name),
-        pod_ip => btl(maps:get(podIP, Status, <<>>)),
-        vxlan_names => string:lexemes(VxlanNames, ?A8nVxlanNamesSep)
-    }, Config)
+process_event_fun(Config) -> fun(Event, State) ->
+    NormalizedEvent = normalize_event(Event),
+    NewState = set_resource_version(NormalizedEvent, State),
+    process_event(NormalizedEvent, Config, NewState)
 end.
 
-process_event(Ev = #{namespace := Namespace, name := Name}, Config) ->
-    ?Log:info(pod_patch(Namespace, Name, Config)).
+process_event(Event, _Config, State) ->
+    ?Log:info(Event),
+    set_resource_version(Event, State).
 
-pod_patch(Namespace, Name, Config) ->
-    ResourcePod = "/api/v1/namespaces/" ++ Namespace ++
-                  "/pods/" ++ Name,
-    {ok, [#{
+normalize_event(#{
+  type := Type,
+    object := #{
       metadata := #{
-        ownerReferences := [#{
-          kind := <<"ReplicaSet">>,
-          name := ReplicaSetName
-        }|_]
+        namespace := Namespace,
+        uid := PodUid,
+        name := PodName,
+        annotations := Annotations,
+        resourceVersion := ResourceVersion
+      },
+      status := Status = #{
+        phase := Phase
       }
-    }]} = ?K8s:http_request(ResourcePod, [], Config),
+    }
+}) -> #{
+    event => list_to_atom(string:lowercase(binary_to_list(Type))),
+    resource_version => binary_to_list(ResourceVersion),
+    namespace => binary_to_list(Namespace),
+    pod_uid => binary_to_list(PodUid),
+    pod_name => binary_to_list(PodName),
+    pod_ip => binary_to_list(maps:get(podIP, Status, <<>>)),
+    vxlan_names => ?Pod:vxlan_names(Annotations),
+    phase => binary_to_list(Phase)
+}.
 
-    ResourceReplicaSet = "/apis/extensions/v1beta1/namespaces/" ++ Namespace ++
-                         "/replicasets/" ++ btl(ReplicaSetName),
-    {ok, [#{
-      metadata := #{
-        ownerReferences := [#{
-          kind := <<"Deployment">>,
-          name := DeploymentName
-        }|_]
-      }
-    }]} = ?K8s:http_request(ResourceReplicaSet, [], Config),
-    ?Log:info(DeploymentName),
-    pod_exec(Namespace, Name, "ip addr show", Config).
+handle_pod_added(Namespace, PodName, PodIp, VxlanNames, _Config) ->
+    ?Log:info("Pod added ~p:", [{Namespace, PodName, PodIp, VxlanNames}]).
 
-pod_exec(Namespace, Name, Command, Config) ->
-    Resource = "/api/v1/namespaces/" ++ Namespace ++ "/pods/" ++ Name ++ "/exec",
-    Query = [{"command", CommandItem} || CommandItem <-
-             string:split(Command, " ", all)] ++ ?AgentExecQuery,
+handle_pod_deleted(Namespace, PodName, PodIp, VxlanNames, _Config) ->
+    ?Log:info("Pod deleted ~p:", [{Namespace, PodName, PodIp, VxlanNames}]).
 
-    {ok, Socket} = ?K8s:ws_connect(Resource, Query, Config),
-    {ok, Result} = ?K8s:ws_recv(Socket),
-    ?K8s:ws_disconnect(Socket),
-    ?Log:info(Result).
+resource_version(State) ->
+    maps:get(resource_version, State, "0").
 
-btl(B) -> binary_to_list(B).
+set_resource_version(#{resource_version := Value}, State) ->
+    OldValue = list_to_integer(resource_version(State)),
+    ProposedValue = list_to_integer(Value),
+
+    case ProposedValue > OldValue of
+        true ->
+            NewValue = integer_to_list(ProposedValue + 1),
+            maps:put(resource_version, NewValue, State);
+        false -> State
+    end.
